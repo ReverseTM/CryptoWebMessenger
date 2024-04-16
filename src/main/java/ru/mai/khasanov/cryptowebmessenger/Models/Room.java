5b0744d54c203451a3d4c4bb8b6db0d26a773e92
Reverse <SteamGamesMy@mail.ru>
2024-04-17 00:16:29 +0300

    Add Room

---
+++ src/main/java/ru/mai/khasanov/cryptowebmessenger/Models/Room.java
@@ -0,0 +1,40 @@
+package ru.mai.khasanov.cryptowebmessenger.Models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "rooms")
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "encryption_algorithm")
+    private String encryptionAlgorithm;
+
+    @Column(name = "cipher_mode")
+    private String cipherMode;
+
+    @Column(name = "padding_mode")
+    private String paddingMode;
+
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(
+            name = "room_user",
+            joinColumns = @JoinColumn(name = "room_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<User> users = new HashSet<>();
+}
--- src/main/java/ru/mai/khasanov/cryptowebmessenger/Models/User.java
+++ src/main/java/ru/mai/khasanov/cryptowebmessenger/Models/User.java
@@ -2,7 +2,6 @@ package ru.mai.khasanov.cryptowebmessenger.Models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
